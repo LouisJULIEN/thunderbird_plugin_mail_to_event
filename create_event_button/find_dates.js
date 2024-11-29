38eca861ab9a46922c5a1802a338fac24347f413
louis JULIEN <louis69600@laposte.net>
2024-11-29 16:55:36 +0100

    test find dates

--- create_event_button/find_dates.js
+++ create_event_button/find_dates.js
@@ -1,29 +1,65 @@
-const mmddyyyyPattern = /\b(?<month>0[1-9]|1[0-2])[/-](?<day>0[1-9]|[12][0-9]|3[01])[/-](?<year>\d{4})\b/g;
-const ddmmyyyyPattern = /\b(?<day>0[1-9]|[12][0-9]|3[01])[/-](?<month>0[1-9]|1[0-2])[/-](?<year>\d{4})\b/g;
-const yyyymmddPattern = /\b(?<year>\d{4})[/-](?<month>0[1-9]|1[0-2])[/-](?<day>0[1-9]|[12][0-9]|3[01])\b/g;
-const mmddPattern = /\b(?<month>0[1-9]|1[0-2])[/-](?<day>0[1-9]|[12][0-9]|3[01])\b/g;
-const ddmmPattern = /\b(?<day>0[1-9]|[12][0-9]|3[01])[/-](?<month>0[1-9]|1[0-2])\b/g;
+const mmddyyyyPattern = /\b(?<month>0[1-9]|1[0-2])[/-](?<day>0[1-9]|[12][0-9]|3[01])[/-](?<year>\d{4})\b/gm;
+const ddmmyyyyPattern = /\b(?<day>0[1-9]|[12][0-9]|3[01])[/-](?<month>0[1-9]|1[0-2])[/-](?<year>\d{4})\b/gm;
+const yyyymmddPattern = /\b(?<year>\d{4})[/-](?<month>0[1-9]|1[0-2])[/-](?<day>0[1-9]|[12][0-9]|3[01])\b/gm;
+const ddmmPattern = /\b(?<day>0[1-9]|[12][0-9]|3[01])[/-](?<month>0[1-9]|1[0-2])\b/gm;
+const mmddPattern = /\b(?<month>0[1-9]|1[0-2])[/-](?<day>0[1-9]|[12][0-9]|3[01])\b/gm;
 
+const punctuationOrSpace = '\.|,|\\s|!'
+const naturalMonthsRegex = `` +
+    `jan[u|${punctuationOrSpace}]|` +
+    `feb[r|${punctuationOrSpace}]|` +
+    `mar[c|${punctuationOrSpace}]|` +
+    `apr[i|${punctuationOrSpace}]|` +
+    `apr[i|${punctuationOrSpace}]|` +
+    `may[${punctuationOrSpace}]|` +
+    `jun[e|${punctuationOrSpace}]|` +
+    `jul[y|${punctuationOrSpace}]|` +
+    `aug[u|${punctuationOrSpace}]|` +
+    `sep[t|${punctuationOrSpace}]|` +
+    `oct[o|${punctuationOrSpace}]|` +
+    `nov[e|${punctuationOrSpace}]|` +
+    `dec[e|${punctuationOrSpace}]` +
+    ``
+
+export const ddmonthPattern = new RegExp(
+    `(?<day>0[1-9]|[12][0-9]|3[01]).*(?<month>(${naturalMonthsRegex})).*(?<year>\\d{4})?.*`,
+    'gim'
+)
+const monthddPattern = new RegExp(
+    `(?<month>(${naturalMonthsRegex})).*(?<day>0[1-9]|[12][0-9]|3[01]).*(?<year>\\d{4})?.*`,
+    'gim'
+)
 
 const allDatesPatternByImportance = [
     ddmmyyyyPattern,
     mmddyyyyPattern,
     yyyymmddPattern,
+    ddmonthPattern,
+    monthddPattern,
     ddmmPattern,
     mmddPattern
 ]
 
+export const splitTextIntoSentences = (text) => {
+    return text.split(/(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|!|\n)\s*/);
+
+}
+
 function findDatesByPattern(text, pattern, extraData) {
     let matches = [];
     let match;
-    while ((match = pattern.exec(text)) !== null) {
-        matches.push({
-            ...extraData,
-            originalText: match[0],
-            regexIndex: match.index,
-            ...match.groups
-        });
-    }
+    const sentences = splitTextIntoSentences(text)
+
+    sentences.map((text) => {
+        while ((match = pattern.exec(text)) !== null) {
+            matches.push({
+                ...extraData,
+                originalText: match[0],
+                regexIndex: match.index,
+                ...match.groups
+            });
+        }
+    })
     return matches;
 }
 
@@ -57,11 +93,7 @@ export function findDates(subjectContent, mailContent) {
         }))
 
     const allFoundDates = [].concat(subjectContentDates, mailContentDates)
-    console.log(allFoundDates)
     const flatFoundDates = [].concat(...allFoundDates);
     const sortedFlatFoundDates = sortDates(flatFoundDates);
-    const filteredDates = removeDuplicateRegexIndex(sortedFlatFoundDates)
-
-    console.log(filteredDates)
-    return filteredDates;
+    return removeDuplicateRegexIndex(sortedFlatFoundDates)
 }
---
+++ tests/find_dates.js
@@ -0,0 +1,47 @@
+import {expect} from "chai";
+import {ddmonthPattern, findDates, splitTextIntoSentences} from "../create_event_button/find_dates.js";
+
+
+describe('find dates', function () {
+    it('should split into sentences', () => {
+
+        const textWithSentences = `This is a sentence. This is another sentence! Is this a question?
+This sentence ends with a line return
+This is a new sentence on a new line.A last one`;
+
+        const sentences = splitTextIntoSentences(textWithSentences)
+        expect(sentences.length).to.equal(6);
+    });
+
+    it('should find natural language dates', () => {
+        let result;
+        result = ddmonthPattern.test('12 janu 2024')
+        expect(result).to.be.true
+        result = ddmonthPattern.test('12 janvier 2024')
+        expect(result).to.be.false
+        result = ddmonthPattern.test('oeoeoe 12 coucou jan salut')
+        expect(result).to.be.true
+    })
+
+    it('should find all dates in this email', () => {
+        const emailContent = 'This is 01/24/2021. This 28 November. This number is alone 2024. 12\n11.'
+        const result = findDates(emailContent, '')
+        expect(result).to.deep.equal([{
+            patternIndex: 1,
+            textSourceIndex: 0,
+            originalText: '01/24/2021',
+            regexIndex: 8,
+            month: '01',
+            day: '24',
+            year: '2021'
+        }, {
+            patternIndex: 3,
+            textSourceIndex: 0,
+            originalText: '28 November.',
+            regexIndex: 5,
+            day: '28',
+            month: 'Nove',
+            year: undefined
+        }])
+    })
+});
