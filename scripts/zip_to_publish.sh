8576e1f30bf80021315beedc90733c9a2619cc06
louis JULIEN <louis69600@laposte.net>
2024-11-30 13:43:51 +0100

    fix package

--- scripts/zip_to_publish.sh
+++ scripts/zip_to_publish.sh
@@ -1,13 +1,16 @@
+#!/bin/bash
 set -e
 
 cd ..
 PLUGIN_VERSION=`jq -r ".version" < manifest.json`
+echo $PLUGIN_VERSION
 PLUGIN_VERSION="${PLUGIN_VERSION//./-}"
-PLUGIN_ZIP_NAME="zip/vinted_autofill-${PLUGIN_VERSION}.zip"
+echo $PLUGIN_VERSION
+PLUGIN_ZIP_NAME="zip/mail_to_event-${PLUGIN_VERSION}.zip"
 
 if [ -f "$PLUGIN_ZIP_NAME" ]; then
     rm "$PLUGIN_ZIP_NAME"
 fi
-zip -r $PLUGIN_ZIP_NAME ./ -x "package*" ".*" "venv" "zip/*" "scripts/*" "node_modules/*" "demo/*"
+zip -r $PLUGIN_ZIP_NAME ./ -x "package*" ".*" "venv" "zip/*" "scripts/*" "node_modules/*"
 
 cd scripts
