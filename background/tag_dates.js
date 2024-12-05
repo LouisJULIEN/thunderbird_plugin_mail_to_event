80513459783b3a71390b07113f019793fa5a59a5
louis JULIEN <louis69600@laposte.net>
2024-12-05 12:16:36 +0100

    new higliht method

--- background/tag_dates.js
+++ background/tag_dates.js
@@ -3,21 +3,29 @@ import {findDates} from "../common/find_dates.js";
 
 export async function tagMailContentDates(mailContentInnerHTML, mailContentPlainText) {
     const foundDates = findDates('', mailContentPlainText, false)
-    let dateIndex = 0
+    let HTMLIndex = 0
     let foundHtmlElements = []
 
     foundDates.map((oneFoundDate) => {
-        console.log(oneFoundDate)
-        const originalText = oneFoundDate.originalDateTimeData.date.originalText
+        const originalDateText = oneFoundDate.originalDateTimeData.date.originalText
 
-        const containerIdValue = `plugin-date-index-${dateIndex}`
-        const dateContainer =
-            `<span id="${containerIdValue}" class="pluginMailToEvent-highlightDate">${originalText}</span>`
+        const containerIdValue = `plugin-date-index-${HTMLIndex}`
+        const dateContainerOpener =
+            `<span id="${containerIdValue}" class="pluginMailToEvent-highlightDate">`
 
-        // .replace(...) only changes the first occurrence
-        mailContentInnerHTML = mailContentInnerHTML.replace(originalText, dateContainer)
+        const dateTextIndex = mailContentInnerHTML.indexOf(originalDateText)
+        if (dateTextIndex !== -1) {
+            let highlightLength = originalDateText.length
 
-        dateIndex += 1
+            const textToHighlight = mailContentInnerHTML.slice(dateTextIndex, dateTextIndex + highlightLength)
+
+            const dateContainer = dateContainerOpener + textToHighlight + '</span>'
+
+            // .replace(...) only changes the first occurrence
+            mailContentInnerHTML = mailContentInnerHTML.replace(textToHighlight, dateContainer)
+        }
+
+        HTMLIndex += 1
         foundHtmlElements.push({
             ...oneFoundDate,
             htmlContainerIdValue: containerIdValue
