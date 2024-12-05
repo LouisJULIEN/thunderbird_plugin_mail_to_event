130dad8dc27b127aa64570d58ede1b1a5b3b8fd4
louis JULIEN <louis69600@laposte.net>
2024-12-05 12:32:41 +0100

    highlight time as well

--- background/tag_dates.js
+++ background/tag_dates.js
@@ -1,6 +1,5 @@
 import {findDates} from "../common/find_dates.js";
 
-
 export async function tagMailContentDates(mailContentInnerHTML, mailContentPlainText) {
     const foundDates = findDates('', mailContentPlainText, false)
     let HTMLIndex = 0
@@ -15,9 +14,24 @@ export async function tagMailContentDates(mailContentInnerHTML, mailContentPlain
 
         const dateTextIndex = mailContentInnerHTML.indexOf(originalDateText)
         if (dateTextIndex !== -1) {
-            let highlightLength = originalDateText.length
+            const textAfterDate = mailContentInnerHTML.slice(dateTextIndex + originalDateText.length)
 
-            const textToHighlight = mailContentInnerHTML.slice(dateTextIndex, dateTextIndex + highlightLength)
+            const originalStartTimeText = oneFoundDate.originalDateTimeData.startTime.originalText
+            const originalEndTimeText = oneFoundDate.originalDateTimeData.endTime.originalText
+
+            const startTimeIndex = textAfterDate.indexOf(originalStartTimeText)
+            const endTimeIndex = textAfterDate.indexOf(originalEndTimeText)
+
+            let timeHighlightOffset = 0
+            if(startTimeIndex !== -1 && startTimeIndex <= 50){
+                timeHighlightOffset = startTimeIndex + originalStartTimeText.length
+            }
+            if(endTimeIndex !== -1 && endTimeIndex <= 70){
+                timeHighlightOffset = Math.max(timeHighlightOffset,
+                    endTimeIndex + originalEndTimeText.length)
+            }
+
+            const textToHighlight = mailContentInnerHTML.slice(dateTextIndex, dateTextIndex + originalDateText.length + timeHighlightOffset)
 
             const dateContainer = dateContainerOpener + textToHighlight + '</span>'
 
