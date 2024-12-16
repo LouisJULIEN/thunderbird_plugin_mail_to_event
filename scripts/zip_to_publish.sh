661ce88a5cfc76beb61c387b1432ff25ba8db748
louis JULIEN <louis69600@laposte.net>
2024-12-16 15:30:13 +0100

    fix zip

--- scripts/zip_to_publish.sh
+++ scripts/zip_to_publish.sh
@@ -10,6 +10,6 @@ PLUGIN_ZIP_NAME="zip/mail_to_event-${PLUGIN_VERSION}.zip"
 if [ -f "$PLUGIN_ZIP_NAME" ]; then
     rm "$PLUGIN_ZIP_NAME"
 fi
-zip -r $PLUGIN_ZIP_NAME ./ -x "package*" ".*" "venv" "zip/*" "scripts/*" "node_modules/*" "bundle_dependencies/*"
+zip -r $PLUGIN_ZIP_NAME ./ -x ".*" "venv" "zip/*" "node_modules/*" "bundle_dependencies/*"
 
 cd scripts
