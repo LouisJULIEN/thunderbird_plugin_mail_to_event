2d1d31427db58f43170a7ed36ba55bb117daf2cd
louis JULIEN <louis69600@laposte.net>
2025-01-13 17:02:50 +0100

    better zip exclusions

--- scripts/zip_to_publish.sh
+++ scripts/zip_to_publish.sh
@@ -10,6 +10,6 @@ PLUGIN_ZIP_NAME="zip/mail_to_event-${PLUGIN_VERSION}.zip"
 if [ -f "$PLUGIN_ZIP_NAME" ]; then
     rm "$PLUGIN_ZIP_NAME"
 fi
-zip -r $PLUGIN_ZIP_NAME ./ -x ".*" "venv" "zip/*" "node_modules/*"
+zip -r $PLUGIN_ZIP_NAME ./ -x ".*" "venv" "node_modules/*" "scripts/*" "zip/*"  "package.json" "package-lock.json"
 
 cd scripts
