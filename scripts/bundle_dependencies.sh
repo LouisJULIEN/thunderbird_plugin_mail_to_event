9f98c16c2859b77b8be2b72aa45cd8292613c1fc
louis JULIEN <louis69600@laposte.net>
2026-03-09 18:25:59 +0100

    fix: bundled dependencies are now ignored

--- scripts/bundle_dependencies.sh
+++ scripts/bundle_dependencies.sh
@@ -1,6 +1,9 @@
 # !/bin/bash
 set -ex
 
-npx esbuild node_modules/franc/index.js --bundle --outfile=dependencies/franc.js --format=esm
-npx esbuild node_modules/@louis.jln/extract-date/index.js --bundle --outfile=dependencies/extract-date.js --format=esm --alias:@=./node_modules/@louis.jln/extract-date
-npx esbuild node_modules/@louis.jln/extract-time/index.js --bundle --outfile=dependencies/extract-time.js --format=esm
+mkdir -p dependencies
+
+# Minify prevents unwanted edits as code can not be read by humans
+npx esbuild node_modules/franc/index.js --bundle --outfile=dependencies/franc.js --format=esm --minify
+npx esbuild node_modules/@louis.jln/extract-date/index.js --bundle --outfile=dependencies/extract-date.js --format=esm --alias:@=./node_modules/@louis.jln/extract-date  --minify
+npx esbuild node_modules/@louis.jln/extract-time/index.js --bundle --outfile=dependencies/extract-time.js --format=esm  --minify
