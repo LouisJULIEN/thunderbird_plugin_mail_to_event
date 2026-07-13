b271e235cfa01880e06081bb73bffaa637629a1f
louis JULIEN <louis69600@laposte.net>
2026-07-13 19:12:33 +0200

    fix: listeners

--- background/register_message_listeners.js
+++ background/register_message_listeners.js
@@ -1,30 +1,34 @@
 import {createEvent} from "../create_event_button/create_calendar_event.js";
 import {findDates} from "../common/find_dates.js";
 
-browser.runtime.onMessage.addListener(async (message) => {
+async function createCalendarEvent(message) {
+    let calendarId = message.calendarId
+    if (!calendarId) {
+        const {defaultCalendarId} = await browser.storage.local.get("defaultCalendarId")
+        calendarId = defaultCalendarId
+    }
+    if (!calendarId) {
+        const calendars = await messenger.calendar.calendars.query({visible: true, readOnly: false, enabled: true})
+        calendarId = calendars[0].id
+    }
+    return createEvent(calendarId, ...message.args)
+}
+
+
+// https://webextension-api.thunderbird.net/en/mv3/guides/runtimeMessaging.html
+browser.runtime.onMessage.addListener((message) => {
     const action = message?.action
     if (action === 'findDates') {
-        return findDates(message.mailSubject, message.mailContentPlainText, message.removeDuplicatesDates, messenger.calendar.timezones.currentZone).dates
+        return Promise.resolve(findDates(message.mailSubject, message.mailContentPlainText, message.removeDuplicatesDates, messenger.calendar.timezones.currentZone).dates)
     }
     else if (action === 'getCalendars') {
         return messenger.calendar.calendars.query({visible: true, readOnly: false, enabled: true})
     }
     else if (action === 'getTimezone') {
-        return messenger.calendar.timezones.currentZone
+        return Promise.resolve(messenger.calendar.timezones.currentZone)
     }
     else if (action === 'createCalendarEvent') {
-        let calendarId = message.calendarId
-        if (!calendarId) {
-            const {defaultCalendarId} = await browser.storage.local.get("defaultCalendarId")
-            calendarId = defaultCalendarId
-        }
-        if (!calendarId) {
-            const calendars = await messenger.calendar.calendars.query({visible: true, readOnly: false, enabled: true})
-            calendarId = calendars[0].id
-        }
-        return createEvent(calendarId, ...message.args)
+        return createCalendarEvent(message)
     }
-    else{
-        throw `Unknown message ${message}`
-    }
+    // Not handled by this listener: return nothing so other listeners can respond.
 })
