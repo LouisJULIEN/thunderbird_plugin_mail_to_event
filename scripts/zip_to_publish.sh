d8b9ef127ee45d76aba1d2b2d58361ed2a6d6a8e
louis JULIEN <louis69600@laposte.net>
2026-07-13 19:21:53 +0200

    feat: webext linter

--- scripts/zip_to_publish.sh
+++ scripts/zip_to_publish.sh
@@ -8,6 +8,6 @@ PLUGIN_ZIP_NAME="zip/mail_to_event-${PLUGIN_VERSION}.zip"
 if [ -f "$PLUGIN_ZIP_NAME" ]; then
     rm "$PLUGIN_ZIP_NAME"
 fi
-zip -r $PLUGIN_ZIP_NAME ./ -x ".*" "venv" "node_modules/*" "scripts/*" "zip/*"  "package.json" "package-lock.json"
+zip -r $PLUGIN_ZIP_NAME ./ -x ".*" "*/.*" "venv" "node_modules/*" "scripts/*" "zip/*"  "package.json" "package-lock.json"
 
 echo $PLUGIN_ZIP_NAME
